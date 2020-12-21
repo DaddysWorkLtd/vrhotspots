34efcd29e7f5d673b6bcae31e27c6e60b7828bde
Paul Cook <paul@daddydev.lan>
2020-12-21 11:17:24 -0800

    added lowdb database

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express=require('express'),
     socket=require('socket.io'),
     SERVER_PORT = 3069,
-    SERVER_NAME = 'localhost',
+    SERVER_NAME = 'daddydev',
+    DATA_FILE = './database/vrvocabdb.json'
     fs = require('fs'),
     https= require('https'),
     key = fs.readFileSync('./config/devkey.pem'),
@@ -15,13 +16,23 @@ const express=require('express'),
         }
     });
 // routes
+const low = require('lowdb'),
+    // use synchronous file mode
+    FileSync = require('lowdb/adapters/FileSync'),
+    db = low(new FileSync(DATA_FILE));
+
+db.defaults({pictures: [], words: {}, uid: 0} ).write();
+
+/* Data structure
+pictures -  src:'', name:'', description: '', parentId:'', [ {hotspotId: '', word:'' , x: 0, y: 0, z:0, radius: 10} ] // may need to be able to specify object type eg circle or rectangular hotposts
+words - ( : '', { description : '',
+                    dutch: { word:'' . description:'' }
+
+ */
 
 
 //static root is public
 app.use(express.static('public'));
-
-
-
 
 var messages = [];
 io.on('connection', (socket) => {
@@ -31,11 +42,14 @@ io.on('connection', (socket) => {
     //messages.forEach( function (old) {
     //    socket.emit('chat', old);
     //});
-
+    console.log( db.get('pictures').find({id:'P1'}).value());
+    db.update('uid', n => n + 1)
+        .write()
     // Handle chat event
     socket.on('addSpot', function(data){
         // console.log(data);
-        //socket.broadcast.emit('chat', data);
+
+
         io.emit('addSpot', data)
         messages.push(data);
         console.log (data, messages.length)
