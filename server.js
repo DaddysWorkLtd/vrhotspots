45bebcbc8bcd9009b940fc7c6f9bd59735546d26
Paul Cook <daddydev@daddyswork.com>
2023-01-29 19:57:44 +0100

    Fixed issues with calculating next reperition time with repeat answers

--- server.js
+++ server.js
@@ -7,38 +7,37 @@ const express = require("express"),
     PRIVATE_DATA = "./database/privatedb.json",
     TRANS_DATA = "./database/transdb.json",
     fs = require("fs"),
-    ip=require('ip'),
-    protocol = process.env.PORT ? require("http") : require("https") , // non secure for glitch which has process.env defined
+    ip = require('ip'),
+    protocol = process.env.PORT ? require("http") : require("https"), // non secure for glitch which has process.env defined
     key = fs.readFileSync("../privkey.pem"),
     cert = fs.readFileSync("../cert.pem"),
     app = express(),
     cors = require("cors")
-    requestIp = require('request-ip'),
+requestIp = require('request-ip'),
     bodyParser = require('body-parser'),
     models = require('./models'),
-     _ = require('lodash');
+    _ = require('lodash');
 
 app.use(cors())
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 //getting the IP address
 //
 app.use(requestIp.mw());
 
 var server;
 // if on glitch dont create a secure server as their proxy does that and it will cause hang ups
-if (!process.env.PORT)
-{
+if (!process.env.PORT) {
     server = protocol
-        .createServer({ key: key, cert: cert }, app)
-        .listen(SERVER_PORT, function() {
+        .createServer({key: key, cert: cert}, app)
+        .listen(SERVER_PORT, function () {
             console.log("https server started on ", SERVER_PORT);
         });
 } else {
     // spin up secure server otherwise cant have webvr
     server = protocol
         .createServer(app)
-        .listen(SERVER_PORT, function() {
+        .listen(SERVER_PORT, function () {
             console.log("http server started on ", SERVER_PORT);
         });
 }
@@ -58,11 +57,11 @@ const low = require("lowdb"),
     db = low(new FileSync(DATA_FILE)),
     udb = low(new FileSync(PRIVATE_DATA))
 const {Sequelize} = require("sequelize");
-    tdb = low(new FileSync(TRANS_DATA))
+tdb = low(new FileSync(TRANS_DATA))
 
-db.defaults({ photos: [], words: {}, uid: 0 }).write();
-udb.defaults({ users: [], log: []}).write();
-tdb.defaults({en_nl:[], nl_en:[]}).write();
+db.defaults({photos: [], words: {}, uid: 0}).write();
+udb.defaults({users: [], log: []}).write();
+tdb.defaults({en_nl: [], nl_en: []}).write();
 
 //static root is public
 app.use(express.static("public"));
@@ -87,32 +86,33 @@ io.on("connection", socket => {
     );*/
 
     // handle logging send everything to db
-    socket.on("log", function(entry) {
-        entry.id=socket.id;
+    socket.on("log", function (entry) {
+        entry.id = socket.id;
         // NEED TO IMPLEMENT SOCKET HANDLER AS MIDDLEWARE TO GET IP from clientIP entry.ip=requestIp.get
         udb.get("log").push(entry).write();
     })
     // Handle add spot event by recording in database and broadcasting out
-    socket.on("addSpot", function(data) {
+    socket.on("addSpot", function (data) {
         // save data
         function getUID() {
             let UID = db.get("uid").value();
             db.update("uid", uid => uid + 1).write();
             // also puts the server ip on it. It's in sync mode so db should lock on each request. Ok for now
-            return( UID.toString() + ip.toLong(ip.address()).toString() );
+            return (UID.toString() + ip.toLong(ip.address()).toString());
         }
+
         // flush the db cashe or it will keep overwriting live edits
         db.read();
         //get the photo and update the spot array
         let rec = db
                 .get("photos")
-                .find({ id: data.photoId }),
+                .find({id: data.photoId}),
             photo = rec.value();
 
-        console.log('add spot',JSON.stringify(data));
+        console.log('add spot', JSON.stringify(data));
         // append spot to array of wordSpots j
         data.id = getUID()
-        photo.wordSpots.push( data );
+        photo.wordSpots.push(data);
 
         // write to database
         rec.assign(photo).write();
@@ -122,31 +122,36 @@ io.on("connection", socket => {
     });
 
 });
-io.on("connect_failed", function() {
+io.on("connect_failed", function () {
     console.log("Connection Failed");
 });
 
-app.get('/api/ping', function(req,res,rrq) {  res.send('pong poo poo')});
+app.get('/api/ping', function (req, res, rrq) {
+    res.send('pong poo poo')
+});
 
-app.get('/api/words/:word/:lang', (req,res,rrq) => res.json( db.get('words').value()[req.params.word][req.params.lang] ));
-app.get('/api/words/:word', (req,res,rrq) => res.json( db.get('words').value()[req.params.word] ));
-app.get('/api/words', (req,res,rrq) => { console.log('words'); res.json( db.get('words').value() )});
-app.get('/api/photos/:id/wordspots', (req,res,rrq) => res.json( db.get('photos').find({id:req.params.id*1}).value().wordSpots ));
-app.get('/api/photos', (req,res,rrq) => res.json( db.get('photos').value() ));
-app.get('/api/photos/:id', (req,res,rrq) => res.json( db.get('photos').find({id:req.params.id*1}).value() ));
+app.get('/api/words/:word/:lang', (req, res, rrq) => res.json(db.get('words').value()[req.params.word][req.params.lang]));
+app.get('/api/words/:word', (req, res, rrq) => res.json(db.get('words').value()[req.params.word]));
+app.get('/api/words', (req, res, rrq) => {
+    console.log('words');
+    res.json(db.get('words').value())
+});
+app.get('/api/photos/:id/wordspots', (req, res, rrq) => res.json(db.get('photos').find({id: req.params.id * 1}).value().wordSpots));
+app.get('/api/photos', (req, res, rrq) => res.json(db.get('photos').value()));
+app.get('/api/photos/:id', (req, res, rrq) => res.json(db.get('photos').find({id: req.params.id * 1}).value()));
 
 const {Translate} = require('@google-cloud/translate').v2;
 const projectId = 'vr-vocab-123';
 const translater = new Translate({projectId});
 
-app.get('/api/translate/:from/:to/:text', async (req,res,rrq) => {
-    let [translation] = await translater.translate(req.params.text, {to: req.params.to,from: req.params.from});
+app.get('/api/translate/:from/:to/:text', async (req, res, rrq) => {
+    let [translation] = await translater.translate(req.params.text, {to: req.params.to, from: req.params.from});
     const coll = req.params.from + "_" + req.params.to;
     tdb.get(coll).push({in: req.params.text, out: translation, ts: new Date()}).write()
-    res.json( translation )
+    res.json(translation)
 });
 
-app.post('/api/translate', async (req,res,rrq) => {
+app.post('/api/translate', async (req, res, rrq) => {
     if (req.body.text) {
         let [translation] = await translater.translate(req.body.text.trim(), {to: req.body.to, from: req.body.from});
         const coll = req.body.from + "_" + req.body.to;
@@ -154,41 +159,45 @@ app.post('/api/translate', async (req,res,rrq) => {
         tdb.get(coll).push({in: req.body.text.trim(), out: translation, ts: new Date()}).write()
         res.json(translation)
     } else {
-        res.json({error:'no in text found'})
+        res.json({error: 'no in text found'})
     }
 });
 
 // api/translate/history/from/to
 
-app.get('/api/history/:from/:to', async (req,res,rrq) => {
+app.get('/api/history/:from/:to', async (req, res, rrq) => {
     try {
-        var words = await models.Word.findAll({where: {fromLang: req.params.from, toLang: req.params.to},
+        var words = await models.Word.findAll({
+            where: {fromLang: req.params.from, toLang: req.params.to},
             order: [['occurances', 'DESC']],
-            attributes: ['fromText','occurances']})
-    }catch (e) {
+            attributes: ['fromText', 'occurances']
+        })
+    } catch (e) {
         console.error(m)
     }
-    res.json( words )
+    res.json(words)
 });
 
-app.get('/api/history/:from/:to/:fromText', async (req,res,rrq) => {
+app.get('/api/history/:from/:to/:fromText', async (req, res, rrq) => {
     try {
-        var words = await models.Word.findAll({where: {fromLang: req.params.from, toLang: req.params.to,fromText: req.params.fromText},
-          attributes: { exclude: ['userId']}})
-    }catch (e) {
+        var words = await models.Word.findAll({
+            where: {fromLang: req.params.from, toLang: req.params.to, fromText: req.params.fromText},
+            attributes: {exclude: ['userId']}
+        })
+    } catch (e) {
         console.error(e)
     }
     if (words) {
         res.json(words)
     } else {
         res.status(404)
-        res.trailer( 'NOT FOUND')
+        res.trailer('NOT FOUND')
     }
 });
 
-app.get('/api/vocably/words/:wordId', async (req,res) => {
+app.get('/api/vocably/words/:wordId', async (req, res) => {
     try {
-        var word = await models.Word.findOne( {where: {wordId: req.params.wordId}} )
+        var word = await models.Word.findOne({where: {wordId: req.params.wordId}})
     } catch (e) {
         res.status(500)
         console.error(e)
@@ -199,13 +208,13 @@ app.get('/api/vocably/words/:wordId', async (req,res) => {
         res.json(word)
     } else {
         res.status(404)
-        res.json( { "NOT_FOUND" :req.params.wordId })
+        res.json({"NOT_FOUND": req.params.wordId})
     }
 })
 
-app.delete('/api/vocably/words/:wordId', async (req,res) => {
+app.delete('/api/vocably/words/:wordId', async (req, res) => {
     try {
-        var deleted = await models.Word.destroy( {where: {wordId: req.params.wordId}} )
+        var deleted = await models.Word.destroy({where: {wordId: req.params.wordId}})
     } catch (e) {
         res.status(500)
         res.json(e)
@@ -215,26 +224,26 @@ app.delete('/api/vocably/words/:wordId', async (req,res) => {
         res.end()
     } else {
         res.status(404)
-        res.json( { "NOT_FOUND" :req.params.wordId })
+        res.json({"NOT_FOUND": req.params.wordId})
     }
 })
 
-app.put('/api/vocably/words/:wordId', async (req,res) => {
+app.put('/api/vocably/words/:wordId', async (req, res) => {
     try {
         // cant update primary key
         delete req.body.wordId
-        var updated = await models.Word.update( req.body, { where: {wordId: req.params.wordId}} )
+        var updated = await models.Word.update(req.body, {where: {wordId: req.params.wordId}})
     } catch (e) {
         res.status(500)
         res.json(e)
     }
     if (updated) {
         // should just get like this, change and save but who cares for now its total mess code anyhoe
-        var word = await models.Word.findOne( {where: {wordId: req.params.wordId}} )
+        var word = await models.Word.findOne({where: {wordId: req.params.wordId}})
         res.json(word)
     } else {
         res.status(404)
-        res.json( { "NOT FOUND" :req.params.wordId })
+        res.json({"NOT FOUND": req.params.wordId})
     }
 })
 
@@ -242,54 +251,63 @@ app.put('/api/vocably/words/:wordId', async (req,res) => {
 
 // create question - can either give choice of answers or give answer and distractors, probably prefer to keep that back. Any hints? These could be keyed b word
 // todo api/question/new?distractors=5
-app.get('/api/vocably/question/:fromLang/:toLang/new', async (req,res) => {
-    function randy(min,max,factor = 1) {
-        const rand=(Math.random() ** factor)
-        return min + Math.floor( rand * (max - min) )
+app.get('/api/vocably/question/:fromLang/:toLang/new', async (req, res) => {
+    function randy(min, max, factor = 1) {
+        const rand = (Math.random() ** factor)
+        return min + Math.floor(rand * (max - min))
     }
+
     // default 3 distractors, can get more
     const distractors = req.query.distractors || 3
     // number of attempts to find unique distactors
     const maxAttempts = 50
     //todo - can only have distractors etc for same language so may need from and to in the url
-    words = await models.Word.findAll({ where: {
-            wordId : { [models.Sequelize.Op.notIn]: [models.sequelize.literal('select word_id from questions where answer_word_id is not null')]},
+    words = await models.Word.findAll({
+        where: {
+            wordId: {[models.Sequelize.Op.notIn]: [models.sequelize.literal('select word_id from questions where answer_word_id is not null')]},
             disabled: null,
-            to_text: {[models.Op.ne] : null},
+            to_text: {[models.Op.ne]: null},
             fromLang: req.params.fromLang,
             toLang: req.params.toLang
         },
-        order: [["occurances","DESC"]]
+        order: [["occurances", "DESC"]]
     });
     let answers = [];
-    let choice = randy(0,words.length-1,1.5) // 1.5 top biases
-    let word = words[choice]
-    answers.push( { wordId: word.wordId, word: word.toText} )
-    let crashCheck=0
-    for ( let i=0; i< ( req.query.distractors || 3); i++ ) {
-        choice = randy(0,words.length-1,.75) // .75 tail biases, more difficult ones
-        // don't include the same word twice unless we're out of words
-        if ( crashCheck < maxAttempts && _.find(answers,{wordId:words[choice].wordId})) {
-            i--
-            crashCheck++
-        } else {
-            answers.push({wordId: words[choice].wordId, word: words[choice].toText})
+    if (!answers.length) {
+        res.json("No new (untested) words found")
+    } else {
+        let choice = randy(0, words.length - 1, 1.5) // 1.5 top biases
+        let word = words[choice]
+        answers.push({wordId: word.wordId, word: word.toText})
+        let crashCheck = 0
+        for (let i = 0; i < (req.query.distractors || 3); i++) {
+            choice = randy(0, words.length - 1, .75) // .75 tail biases, more difficult ones
+            // don't include the same word twice unless we're out of words
+            if (crashCheck < maxAttempts && _.find(answers, {wordId: words[choice].wordId})) {
+                i--
+                crashCheck++
+            } else {
+                answers.push({wordId: words[choice].wordId, word: words[choice].toText})
+            }
         }
+        question = await models.Question.create({wordId: answers[0].wordId, distractors: distractors, reverse: false})
+        const returnObj = {
+            questionId: question.questionId,
+            word: word.fromText, // if not reverse
+            choices: _.shuffle(answers)
+        }
+        res.json(returnObj)
     }
-    question = await models.Question.create( {wordId: answers[0].wordId, distractors: distractors, reverse: false})
-    const returnObj = { questionId: question.questionId,
-        word: word.fromText, // if not reverse
-        choices: _.shuffle(answers) }
-    res.json(returnObj)
 })
 
-app.get('/api/vocably/question/:fromLang/:toLang/repeat', async (req,res) => {
+app.get('/api/vocably/question/:fromLang/:toLang/repeat', async (req, res) => {
     // get a due question - if no repeat questions return none or maybe a flag to override
     // default 3 distractors, can get more
     const distractors = req.query.distractors || 3
     // todo: not going to have enoughd distractors, these can be from anywhere
-    wordsLearn = await models.WordLearning.findAll({ where: {
-            wordId : {
+    wordsLearn = await models.WordLearning.findAll({
+        where: {
+            wordId: {
                 [models.Sequelize.Op.in]: [models.sequelize.literal("SELECT words.word_id FROM questions,words \
                                                     WHERE words.word_id=questions.word_id \
                                                     AND words.disabled is NULL \
@@ -298,23 +316,26 @@ app.get('/api/vocably/question/:fromLang/:toLang/repeat', async (req,res) => {
                                                     AND to_lang='" + req.params.toLang + "'")]
             },
         },
-        order: [[ 'nextRepetition', 'ASC']],
+        order: [['nextRepetition', 'ASC']],
 //        order: models.sequelize.random(),
-        limit:distractors+1 })
+        limit: distractors + 1
+    })
 
-    question = await models.Question.create( {wordId: wordsLearn[0].wordId, distractors: distractors, reverse: false})
+    question = await models.Question.create({wordId: wordsLearn[0].wordId, distractors: distractors, reverse: false})
     var _questionWord
-    let answerList = await Promise.all(wordsLearn.map(async function(wordLearn){
+    let answerList = await Promise.all(wordsLearn.map(async function (wordLearn) {
         word = await models.Word.findByPk(wordLearn.wordId)
-        return { wordId: word.wordId, word: word.toText, fromText: word.fromText };
+        return {wordId: word.wordId, word: word.toText, fromText: word.fromText};
     }));
-    const returnObj = { questionId: question.questionId,
-        word: _.find(answerList,{wordId: question.wordId}).fromText, // if not reverse
-        choices: _.shuffle(_.omit(answerList,'fromText')) }
+    const returnObj = {
+        questionId: question.questionId,
+        word: _.find(answerList, {wordId: question.wordId}).fromText, // if not reverse
+        choices: _.shuffle(_.omit(answerList, 'fromText'))
+    }
     res.json(returnObj)
 })
-app.put('/api/vocably/answer/:questionId', async (req,res) => {
-    const question = await models.Question.findByPk( req.params.questionId )
+app.put('/api/vocably/answer/:questionId', async (req, res) => {
+    const question = await models.Question.findByPk(req.params.questionId)
     if (!question) {
         res.status(404)
         return res.json({"QUESTION NOT FOUND": req.params.questionId})
@@ -326,28 +347,28 @@ app.put('/api/vocably/answer/:questionId', async (req,res) => {
     }
     let confidence = req.body.confidence || .5
     // todo: do not allow old questions to be updated, as PKs are guessable
-    const word = await models.Word.findByPk(  req.body.wordId )
+    const word = await models.Word.findByPk(req.body.wordId)
     if (!word) {
         res.status(404)
         return res.json({"ANSWER NOT FOUND": req.body.wordId})
     }
     question.answerWordId = req.body.wordId
-    let correct = await question.processAnswer( req.body.wordId , confidence)
-    res.status(correct ? 200: 400)
+    let correct = await question.processAnswer(req.body.wordId, confidence)
+    res.status(correct ? 200 : 400)
     let correctWrong = correct ? "correct" : "incorrect"
-    console.log('correct',correct,correctWrong)
-    const returnValue={}
-    returnValue[correctWrong]=question.wordId
+    console.log('correct', correct, correctWrong)
+    const returnValue = {}
+    returnValue[correctWrong] = question.wordId
     return res.json(returnValue)
 })
 // deletes question and disables word so it's not asked again
-app.delete('/api/vocably/question/:questionId', async (req,res) => {
-    question=await models.Question.findByPk(req.params.questionId)
+app.delete('/api/vocably/question/:questionId', async (req, res) => {
+    question = await models.Question.findByPk(req.params.questionId)
     if (!question) {
         res.status(404)
         return res.json({"QUESTION NOT FOUND": req.params.questionId})
     } else {
-        word=await models.Word.findByPk(question.wordId)
+        word = await models.Word.findByPk(question.wordId)
         word.disabled = new Date()
         word.save()
         question.destroy()
@@ -357,6 +378,6 @@ app.delete('/api/vocably/question/:questionId', async (req,res) => {
 });
 
 // for current user todo implement
-app.get('/api/logs', (req,res,rrq) => res.json( udb.get('logs').value() ));
+app.get('/api/logs', (req, res, rrq) => res.json(udb.get('logs').value()));
 
 console.log('server.js complete')
