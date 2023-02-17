5fcf8d067ce795bed9a6ab1f34a0604a5d667f25
Paul Cook <daddydev@daddyswork.com>
2023-02-17 11:58:29 +0100

    Automate non-secure on my-ass

--- client/vocabby/vue.config.js
+++ client/vocabby/vue.config.js
@@ -1,8 +1,13 @@
+const os = require('os')
+
 module.exports = {
   devServer: {
     // public: 'vocab.daddyswork.com:80' - should work, not tested
     disableHostCheck: true,
     // got a problem on myass, needs to be false. Should set environment variable
-    https: true
-  }
+    https: os.hostname() == 'my-ass' ? false : true
+  },
+  disableHostCheck: false,
+  // done by apache
+  https: false
 }
